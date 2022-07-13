88e687cc444039e30ae426329a062f4a0025cbab
Thalassa Plack <thalassa@il-e3.msk.21-school.ru>
2022-07-13 19:49:16 +0300

    add flags -b -s -n

--- src/cat.c
+++ src/cat.c
@@ -9,7 +9,7 @@ int flags_status(flags *status) {
   status->s = 0;
   status->T = 0;
   return 0;
-};
+}; 
 
 int getFlags(flags *status, char f) {
   int error = 0;
@@ -45,12 +45,75 @@ int getFlags(flags *status, char f) {
 }
 
 int main(int ARGC, char **ARGV) {  //  подается  s21_cat -l test.txt
-  int count = 0;
-  if (ARGC > 1) {
-    while (ARGV) {
-      if (ARGV[count][0] == '-') break;
-    }
-  }
+  // int count = 0;
+  // if (ARGC > 1) {
+  //   while (ARGV) {
+  //     if (ARGV[count][0] == '-') break;
+  //   }
+  // }
 
+  // fscanf(f, "%s", c);
+
+}
+
+
+int flag_n() {
+  FILE *f;
+  char c[1000];
+  int i = 1;
+  f = fopen("test_file.txt", "r");
+	while(!feof(f)) {
+		if (fgets(c, 1000, f) != NULL) {
+				if (c[0] == '\n') {
+					printf("%8s", c);
+				} else {
+			printf("%6d%2s", i, c);
+			i++;
+			}
+		}
+	}
+  fclose(f);
+  return 0;
+}
+
+int flag_b() {
+  FILE *f; 
+  char c[1000];
+  int i = 1;
+  f = fopen("test_file.txt", "r");
+	while(!feof(f)) {
+		if (fgets(c, 1000, f) != NULL) {
+			printf("%6d%2s", i, c);
+			i++;
+		}
+	}
+  fclose(f);
+  return 0;
+} 
+
+int flag_s() {
+  FILE *f;
+  char c[1000];
+  int count = 0;
+  int double_print = 0;
+  f = fopen("test_file.txt", "r");
+	while(!feof(f)) {
+		if (fgets(c, 1000, f) != NULL) {
+			if (c[0] == '\n') {
+				count++;
+			}
+			if (count == 2) {
+				count = 1;
+			} else {
+				printf("%s", c);
+				double_print++;
+				if (double_print == 2) { 
+					count = 0;
+					double_print = 0;
+				}
+			}
+		}
+	}
+  fclose(f);
   return 0;
 }
